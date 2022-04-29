313bed1e8ecabce3a71b99b34da125b7e5b35239
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2022-04-29 01:34:36 -0300

    controllers, routes and models by Users and Publications with TypeScript

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from "./app"
 import './database'
 
 app.listen(app.get('port'), () => {
-    console.log(`Server en puerto ${app.get('port')}`) 
+    console.log(`Server on port ${app.get('port')} my friend`) 
 })
