da71d8f2bcdb481f1adb284e6ad89254529e3448
Joel <joeljuliandurand@gmail.com>
2022-09-29 16:28:30 -0300

    .

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import server from "./app"
-import './database'
+import server from "./src/app"
+import './src/database'
 
 server.listen(8080, () => {
     console.log(`app on port 8080`) 
