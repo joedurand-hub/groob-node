c3a294b5cac1a98db69d787136613eef45f15022
Joel <joeljuliandurand@gmail.com>
2022-09-29 00:09:28 -0300

    new configuration in atlas

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import mongoose, { ConnectOptions } from "mongoose";
 (async () => {
         try {
-    const URI = `mongodb+srv://${process.env.MONGO_DATABASE}:${process.env.MONGO_PASSWORD}@cluster0.p7kqs.mongodb.net/test?retryWrites=true`
+    const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.p7kqs.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true`
     await mongoose.connect(URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
