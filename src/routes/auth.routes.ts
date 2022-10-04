7620caa4019a43583a8db2273badeb665fdf574b
Joel <joeljuliandurand@gmail.com>
2022-10-03 21:51:27 -0300

    token in logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { signup, login, logout } from '../controllers/auth.controller';
 import { schemaValidation } from '../libs/schemasValidator';
 import { LoginSchema, SignupSchema } from '../schemas/auth..schema';
+import { TokenValidator } from '../libs/tokenValidator';
 
 const router = Router()
 
@@ -9,7 +10,7 @@ router.post('/signup', schemaValidation(SignupSchema), signup)
 
 router.post('/login', schemaValidation(LoginSchema), login)
 
-router.post('/logout', logout)
+router.post('/logout', TokenValidator, logout)
 
 // router.post('/reset', TokenValidator, reset)
 
