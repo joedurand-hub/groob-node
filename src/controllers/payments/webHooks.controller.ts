9b88266a83cd3bfceda8da408862371a700ebe9f
Joel <joeljuliandurand@gmail.com>
2022-10-12 22:20:23 -0300

    webHooks

--- src/controllers/payments/webHooks.controller.ts
+++ src/controllers/payments/webHooks.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 // import mercadopago from 'mercadopago'
 
-export const mPayment = async (req: Request, res: Response) => {
+export const webHook = async (req: Request, res: Response) => {
     try {
         console.log(req.body)
         res.status(200).json("Todo piola")
--- src/routes/payments.routes.ts
+++ src/routes/payments.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { mPayment } from '../controllers/payments/checkout.controller'
+import { webHook} from '../controllers/payments/webHooks.controller'
 
-import { TokenValidator } from '../libs/tokenValidator';
+// import { TokenValidator } from '../libs/tokenValidator';
 
 const router = Router()
 
 router.post('/checkout',  mPayment)
 
-router.post('/notifications',  mPayment)
+router.post('/notifications',  webHook)
 
 export default router;
