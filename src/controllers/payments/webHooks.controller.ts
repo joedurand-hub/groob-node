621651854be6e879bac8537f8eeda25d5d2f8ad7
Joel <joeljuliandurand@gmail.com>
2022-10-13 22:45:09 -0300

    ..

--- src/controllers/payments/webHooks.controller.ts
+++ src/controllers/payments/webHooks.controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from "express";
+import fs from "fs"
 import Sales from "../../models/Sales";
 // import mercadopago from 'mercadopago'
 
 export const webHook = async (req: Request, res: Response) => {
     try {
-        const all = req.body
-        const pay = new Sales({pay: all})
-        console.log(pay)
-        res.status(200).json(pay)
+
+        console.log(req.body)
+        let data = req.body
+
+        if (data['action'] == 'payment.created') {
+            fs.writeFileSync(`${__dirname}/../webHookResponses.json`, JSON.stringify(data))
+        }
+
+        res.status(200).send({ result: 'ok', message: 'Gracias' })
     } catch (error) {
         console.log(error)
     }
-  
-  }
+
+}
