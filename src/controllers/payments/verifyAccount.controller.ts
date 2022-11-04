91e1d8f4ef50918adb965b0133613e2b4a30a5ff
Joel <joeljuliandurand@gmail.com>
2022-11-04 03:51:36 -0300

    buy verification added

---
+++ src/controllers/payments/verifyAccount.controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import mercadopago from 'mercadopago'
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN_PRUE_MP
+});
+
+export const verifyAccountPay = async (req: Request, res: Response) => {
+  const { idToVerify, title, price, quantity, picURL, descripcion, 
+    nombre, apellido, email, direccion, numeroDireccion, area, tel, postal, } = req.body
+    console.log(idToVerify, title, price, quantity, picURL, descripcion, 
+        nombre, apellido, email, direccion, numeroDireccion, area, tel, postal, )
+  
+  const cantidad = parseInt(quantity)
+  const precio = parseInt(price)
+
+  try {
+
+    let preference = {
+      items: [
+        {
+          id: idToVerify,
+          title: title,
+          unit_price: precio,
+          quantity: cantidad,
+          description: descripcion,
+          currency_id: "ARS",
+          picture_url: picURL,
+        }
+      ],
+      payer: {
+        "name": nombre,
+        "surname": apellido,
+        "email": email,
+        "phone": {
+          "area_code": area,
+          "number": tel
+        },
+        "address": {
+          "zip_code": postal,
+          "street_name": direccion,
+          "street_number": numeroDireccion
+        },
+
+      },
+      
+      back_urls: {
+        "success": "https://groob.com.ar/notifications/success",
+        "pending": "https://groob.com.ar/notifications/pending",
+        "failure": "https://groob.com.ar/notifications/error",
+      },
+      auto_return: "approved",
+      notification_url: "https://groob-backend-production.up.railway.app/notifications",
+    };
+
+    mercadopago.preferences.create(preference)
+      .then((response) => {
+        console.log(response)
+        res.json(response)
+      })
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ message: error });
+  }
+
+}
--- src/routes/payments.routes.ts
+++ src/routes/payments.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
-import { mPayment } from '../controllers/payments/checkout.controller'
+// import { mPayment } from '../controllers/payments/checkout.controller'
+import { verifyAccountPay } from '../controllers/payments/verifyAccount.controller'
 import { webHook} from '../controllers/payments/webHooks.controller'
 
 // import { TokenValidator } from '../libs/tokenValidator';
 
 const router = Router()
 
-router.post('/prefer-verify-account',  mPayment)
+router.post('/prefer-verify-account',  verifyAccountPay)
+// router.post('/prefer-product',  verifyAccountPay)
 
 router.post('/notifications',  webHook)
 
