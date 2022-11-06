6339e0a82cd3a929791d41f3417c65bae951c0a2
Joel <joeljuliandurand@gmail.com>
2022-11-06 18:33:47 -0300

    order by verified

--- src/controllers/interaction/searchUser.controller.ts
+++ src/controllers/interaction/searchUser.controller.ts
@@ -7,19 +7,24 @@ export const searchUser = async (req: Request, res: Response) => {
         console.log(input)
         if (input === undefined || input === null || input === "") return
         else {
-            let data = await User.find()
-        const result = data.filter(user => {
-            if(user.userName.toLowerCase().includes(input)
-            || user.description.toLowerCase().includes(input)
-            || user.firstName.toLowerCase().includes(input)
-            || user.lastName.toLowerCase().includes(input)
-            || user.email.toLowerCase().includes(input)
-            ) {
-                return user
-            }
-        })
-        return res.status(200).json(result)
-    }
+            let users = await User.find()
+            const result = users.filter(user => {
+                if (user.userName.toLowerCase().includes(input)
+                    || user.description.toLowerCase().includes(input)
+                    || user.firstName.toLowerCase().includes(input)
+                    || user.lastName.toLowerCase().includes(input)
+                    || user.email.toLowerCase().includes(input)
+                ) {
+                    return user
+                }
+            }).sort((a: any, b: any) => {
+                if (a.verified < b.verified) return 1;
+                if (a.verified > b.verified) return -1;
+            })
+
+            res.status(200).json(result)
+            return res.status(200).json(result)
+        }
     } catch (error) {
         console.log(error)
     }
