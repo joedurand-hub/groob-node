828c0e81caeb3d4768fdba4199293fc626a4b7ad
Joel <joeljuliandurand@gmail.com>
2022-11-11 13:56:42 -0300

    ..

--- src/routes/payments.routes.ts
+++ src/routes/payments.routes.ts
@@ -7,7 +7,7 @@ import { TokenValidator } from '../libs/tokenValidator';
 
 const router = Router()
 
-router.post('/prefer-verify', verifyAccountPay)
+router.post('/prefer-verify', TokenValidator, verifyAccountPay)
 router.post('/prefer-product', TokenValidator, productPayment)
 
 router.post('/notifications',  webHook)
