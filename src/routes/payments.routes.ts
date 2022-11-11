dce20e8ade72d57f52fb0e3d5792e386bc35bdc1
Joel <joeljuliandurand@gmail.com>
2022-11-11 17:19:41 -0300

    ..

--- src/routes/payments.routes.ts
+++ src/routes/payments.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 import { productPayment } from '../controllers/payments/product/product.controller'
-import { verifyAccountPay } from '../controllers/verifyAccount.controller'
+import { verificationPay } from '../controllers/verification.controller'
 import { webHook} from '../controllers/payments/verifyAccount/webHook.controller'
 
 import { TokenValidator } from '../libs/tokenValidator';
 
 const router = Router()
 
-router.post('/verifyAccount', TokenValidator, verifyAccountPay)
-router.post('/productPreference', TokenValidator, productPayment)
+router.post('/verificationPay', TokenValidator, verificationPay)
+// router.post('/productPreference', TokenValidator, productPayment)
 
-router.post('/notifications',  webHook)
+// router.post('/notifications',  webHook)
 // router.post('/notifications',  webHook)
 
 export default router;
